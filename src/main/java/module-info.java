d098d59ca475c19793f7e7d34b3d0c710cd5cfb2
Ethan McCue <ethan@mccue.dev>
2025-02-02 02:27:53 -0500

    Update versions and provide Module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,11 @@
+import com.fasterxml.jackson.databind.Module;
+import dev.mccue.json.jackson.JsonModule;
+
 module dev.mccue.json.jackson {
     requires transitive com.fasterxml.jackson.databind;
     requires transitive dev.mccue.json;
 
     exports dev.mccue.json.jackson;
+
+    provides Module with JsonModule;
 }
